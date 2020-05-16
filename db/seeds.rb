d6f1fe007daa8abb9377dac904ed1830ea4496cb
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-16 14:47:44 +0800

    Seeded 10 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up database"
+Restaurant.destroy_all
+
+puts "Start seeding"
+
+count = 1
+
+10.times do
+  Restaurant.create!(name: "restaurant #{count}",
+                     address: "#{count} Orchard Road",
+                     category: %w[chinese italian japanese french belgian].sample,
+                     phone_number: "1234 1234"
+                    )
+  count += 1
+end
+puts "Created #{count - 1} restaurants!"
