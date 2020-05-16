93730d4c66da3f2196e425ab30d3f8ff0736cceb
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-16 15:14:13 +0800

    review validation added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian]}
+
+  #relationship
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  # validation
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true },
+            inclusion: { in: (0..5) }
+  # relationship
   belongs_to :restaurant
 end
