401aaba438639a421ad2ab255237e1be9bac55fc
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-17 13:26:28 +0800

    add p tag to link_to

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new()
+    @review = Review.new
   end
 
   def create
